6af6a46408d7957a19591ab52844393bbcf794df
panda <panda.chen.2021@gmail.com>
2022-12-24 01:47:33 +0000

    add snapshot to change dnd style

--- src/columnList.js
+++ src/columnList.js
@@ -13,20 +13,30 @@ const TitleCol = styled.h2`
     padding: 12px;    
     text-align: center;
 `;
-const TodoTask = styled.div``;
+const TodoTask = styled.div`
+    padding: 8px;
+`;
+
+//***Questions:
+// */ I have no idea where is the upper level of 'isDraggingOver'. Props.isDraagingOver undef, but below snapshot.isDragging Over works!
+//border: ${props => (props.isDraggingOver ? '3px solide lightyellow' : 'lightgray')};
 
 export default function ColumnList({col, tasks}) {
     //console.dir(<i class="fa fa-tasks" aria-hidden="true"></i>);
 
     return(
         <Droppable droppableId={col.id}>
-            {provided => (
+            {(provided, snapshot) => (
                 <Container 
                     ref = {provided.innerRef}
                     {...provided.droppableProps}
+                    style = {{border: snapshot.isDraggingOver ? '6px solid lightgray' : 'none'}}
+                    //****Using style ={...} way to solve the problem that can't find the upper level */
+                    //isDraggingOver = {snapshot.isDraggingOver}
                 >
                     <TitleCol>{col.title}</TitleCol>
                     <TodoTask>{tasks.map((task, index)=> <TaskList key={task.id} taskObj={task} index={index}></TaskList>)}</TodoTask>
+                    {console.dir(Droppable.snapshot)}
                     { provided.placeholder }
                 </Container>
             )}
--- src/taskList.js
+++ src/taskList.js
@@ -5,17 +5,19 @@ import { Draggable } from "react-beautiful-dnd";
 const TaskItem = styled.div`
 margin: 12px;   
 padding: 12px;
-border: 1px solid lightgray;
-`;
+transition: background 0.2s ease;
+border: ${props => (props.isDragging ? '3px solid lightblue' : '1px solid lightgray') };
+`
 
 export default function TaskList({ taskObj, index }) {
     return(
         <Draggable draggableId={taskObj.id} index={index}>
-            {provided => (
+            {(provided, snapshot) => (
                 <TaskItem
                     { ...provided.draggableProps }
                     { ...provided.dragHandleProps }
                     ref = { provided.innerRef }
+                    isDragging = { snapshot.isDragging }
                 >
                     { taskObj.title }
                 </TaskItem>
